329427067bf781873806b4a66a555f7841ebe9ec
bitsocketsrl <sergio.casolari@scasolari.com>
2018-02-14 18:50:21 +0100

    p

--- stefanotriulzi/development.py
+++ stefanotriulzi/development.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '29-ug6^0wv(=ii3ywnd6csk@&izhwa6h13$mm)gvhq5a7&31b^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'snippetcode.pythonanywhere.com', '127.0.0.1']
 
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'media/')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
