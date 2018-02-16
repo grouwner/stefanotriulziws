08fe1ea9c363ca05da05400c881af723ef16c59f
bitsocketsrl <sergio.casolari@scasolari.com>
2018-02-16 17:47:35 +0100

    p

--- stefanotriulzi/settings.py
+++ stefanotriulzi/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 EMAIL_SENDER_ADDRESS = None
 
